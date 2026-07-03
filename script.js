c05e0326eb17dce1aaf644caa992b0d6e8e53cc5
Munim <muhtasimhaque2@gmail.com>
2026-07-04 02:35:16 +0600

    initiate js

---
+++ script.js
@@ -0,0 +1,7 @@
+function showWelcomeMessage() {
+    alert("Welcome to Artisan Coffee! We are glad you are here.");
+}
+
+function addToCart() {
+    alert("Item successfully added to your cart!");
+}
